55df5fa178617fd966925938ef8c0ec47d2990a1
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-09 18:27:14 -0300

    Primeira aula.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var nome = "Rodolpho Erick";
+var idade = 29;
+alert("A idade de " + nome + " é " + idade + " anos.");
+
+var primeiroValor = 3;
+var segundoValor = 4.5;
+alert("Exemplo de soma: 3 + 4.5 = " + (primeiroValor + segundoValor));
+
+
+var primeiroValorString = "3";
+var segundoValorString = "4.5";
+alert("Exemplo de concatenação de Strings: 3 + 4.5 = " + primeiroValorString + segundoValorString);
+
+var frase = "Japão é a melhor seleção do mundo!";
+
+// Exemplo de console.log() para visualizar no Console (F12).
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
