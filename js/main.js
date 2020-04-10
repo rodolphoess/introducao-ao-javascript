3b9fb2ad88f6c42a49eac9c16c5dcf96cb2647cb
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-09 22:18:50 -0300

    Segunda aula curso.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var nome = "Rodolpho Erick";
 var idade = 29;
-alert("A idade de " + nome + " é " + idade + " anos.");
+// alert("A idade de " + nome + " é " + idade + " anos.");
 
 var primeiroValor = 3;
 var segundoValor = 4.5;
-alert("Exemplo de soma: 3 + 4.5 = " + (primeiroValor + segundoValor));
+// alert("Exemplo de soma: 3 + 4.5 = " + (primeiroValor + segundoValor));
 
 
 var primeiroValorString = "3";
 var segundoValorString = "4.5";
-alert("Exemplo de concatenação de Strings: 3 + 4.5 = " + primeiroValorString + segundoValorString);
+// alert("Exemplo de concatenação de Strings: 3 + 4.5 = " + primeiroValorString + segundoValorString);
 
 var frase = "Japão é a melhor seleção do mundo!";
 
@@ -18,3 +18,23 @@ console.log(nome);
 console.log(idade);
 console.log(frase.replace("Japão", "Brasil"));
 console.log(frase.toUpperCase());
+
+var lista = ["Maçã", "Pêra", "Laranja"];
+console.log(lista);
+console.log(lista.reverse()); //Imprime os elementos da lista em ordem controária à inserção.
+lista.push("Uva");
+console.log(lista);
+lista.pop(); //Retira o último elemento da lista.
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.toString()[0]); //Retorna cada letra da lista transformada em String.
+console.log(lista.join(" - ")); //Transforma a lista em String e também é possível adicionar o separador entre elementos da lista.
+
+var fruta = { nome: "Maçã", cor: "Vermelha" }; //Dicionário ou objeto JSON.
+console.log(fruta);
+console.log(fruta.nome);
+
+var frutas = [{ nome: "Maçã", cor: "Vermelha" }, { nome: "Uva", cor: "Roxa" }]; //Dicionários ou objetos JSON.
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[1]);
