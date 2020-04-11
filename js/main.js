242a4b1b7c95b5dd95f29ed95e816c9ef29fc645
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-10 22:22:58 -0300

    Código aula 4.

--- js/main.js
+++ js/main.js
@@ -51,3 +51,22 @@ var list = ["Alemanha", "Inglaterra", "Escócia"];
 list.push("Polônia");
 list.pop("Inglaterra");
 console.log(list);
+
+function soma(n1, n2) {
+    return n1 + n2;
+};
+console.log(soma(1, 2));
+
+
+function validaIdade(idade) {
+    var validar;
+    if (idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+
+    return validar;
+};
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
